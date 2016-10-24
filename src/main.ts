1c41b4fa2be220e07c08d25f947073b3745dba40
Lucian <lucian.mbraz@gmail.com>
2016-10-24 07:37:11 -0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Aurelia } from 'aurelia-framework';
+
+export function configure(aurelia: Aurelia) {
+
+  return System.import('materialize').then(() => {
+
+    aurelia.use
+      .standardConfiguration()
+      .developmentLogging();
+
+    // Uncomment the line below to enable animation.
+    // aurelia.use.plugin('aurelia-animator-css');
+
+    // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    // aurelia.use.plugin('aurelia-html-import-template-loader')
+
+    aurelia.use.plugin('aurelia-materialize-bridge', bridge => bridge.useAll());
+
+    return aurelia.start().then(() => aurelia.setRoot());
+  });
+}
